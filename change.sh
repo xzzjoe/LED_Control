720c8e5b0160ecc05f90ed7d080a57ae6e36391e
xzzjoe <xzzjoe@bu.edu>
2023-04-25 19:12:58 -0400

    quick bug fixes

--- change.sh
+++ change.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 echo $1 > /sys/class/pwm/pwm-4:0/duty_cycle
 echo 1 > /sys/class/pwm/pwm-4:0/enable
--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo -n pwm > /sys/devices/platform/ocp/ocp:P9_14_pinmux/state
-echo 0 > sys/class/pwm/pwmchip4/export
+echo 0 > /sys/class/pwm/pwmchip4/export
 echo 100000 > /sys/class/pwm/pwm-4:0/period
