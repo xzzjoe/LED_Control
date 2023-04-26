6503125cb1a15da1f5074320c58c751a16a8b8d7
Ryan Rosenberger <rjrose@bu.edu>
2023-04-25 23:36:31 -0400

    Working timeout functionality

--- GUI/brightnessControl/plusminus.cpp
+++ GUI/brightnessControl/plusminus.cpp
@@ -1,10 +1,10 @@
-//Credit to https://zetcode.com/gui/qt5/firstprograms/ for the initial button plus minus code
+﻿//Credit to https://zetcode.com/gui/qt5/firstprograms/ for the initial button plus minus code
 #include "plusminus.h"
 #include <QGridLayout>
-
 PlusMinus::PlusMinus(QWidget *parent)
     : QWidget(parent) {
     val = 0;
+    numTimers=0;
     auto *plsBtn = new QPushButton("Increase Brightness", this);
     auto *minBtn = new QPushButton("Decrease Brightness", this);
     lbl = new QLabel("Brightness: " + QString::number(val) + "%", this);
@@ -50,9 +50,15 @@ void PlusMinus::updateVal(){
     bar->setValue(val);
     int dutyCycle = val*1000;
     QProcess::startDetached("/root/change.sh", QStringList {QString::number(dutyCycle)});
-    std::ofstream km;
-    km.open(fileName);
-    km << std::to_string(val);
-    km.close();
+    //Every time updateVal is called, a single shot time out timer is created. Since they'll
+    //overlap, and we only want the lights to turn off after the last timer expires, we keep
+    //track of the number of timers and only turn off the lights once no timers are left.
+    numTimers++;
+    QTimer::singleShot(timeOutDuration*1000,this,SLOT(OnTimeOut()));
+
+}
+void PlusMinus::OnTimeOut(){
+    numTimers--;
+    if(numTimers==0) QProcess::startDetached("/root/change.sh", QStringList {QString::number(0)});
 }
 
