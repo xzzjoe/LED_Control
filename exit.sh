44ea3c3d6596d0e12c826aafcc889d547c3afcb8
xzzjoe <71287197+xzzjoe@users.noreply.github.com>
2023-04-26 17:22:30 -0400

    shell script changes

--- exit.sh
+++ exit.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo 0 > /sys/class/pwm/pwmchip4/pwm-4:0/enable
 echo 0 > /sys/class/pwm/pwmchip4/unexport
+echo 66 > /sys/class/gpio/unexport 
--- init_gpio.sh
+++ init_gpio.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo 66 > /sys/class/gpio/export
-echo -n out > /sys/class/gpio/gpio66/direction
-echo -n rising > /sys/class/gpio/gpio66/edge
+cd /sys/class/gpio/gpio66
+echo -n out > direction
+echo -n rising > edge
